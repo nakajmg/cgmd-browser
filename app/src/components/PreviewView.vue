e65def54956497655e380f5e718d95360905655d
nakajmg <nakajima.jmg@gmail.com>
2016-09-25 05:43:53 +0900

    スクロール位置の保持修正

--- app/src/components/PreviewView.vue
+++ app/src/components/PreviewView.vue
@@ -80,6 +80,7 @@
         if (this.scroll[this.currentFilePath] !== undefined) {
           scroll = this.scroll[this.currentFilePath]
         }
+        console.log(scroll)
         this.$refs.preview.executeJavaScript(`
           update('${escape(md)}',${scroll})
         `)
